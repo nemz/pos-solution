a779e8661fb33094f56b029179cc7280899a7399
angelfeliz <erodriguezfeliz@gmail.com>
2016-05-16 23:59:30 -0430

    agregue la calse IdentificationType, PosContext  agregue la DLL de EF al proyecto existe una DLL que me esta dando erro de referencia "FluentValidation.Attributes" nunca la he usado, agregue la propiedad de acceso Public a las clases agregue campo Id a Customer. commente un Enum en Customer coloque comentario

--- ModernPOS.WebAPI/ModernPOS.WebAPI/Models/IdentificationType.cs
+++ ModernPOS.WebAPI/ModernPOS.WebAPI/Models/IdentificationType.cs
@@ -5,15 +5,17 @@ using System.Text;
 
 namespace ModernPOS.WebAPI.Models
 {
-    class IdentificationType
+    public class IdentificationType
     {
         public int Id { get; set; }
-
-        public string   Name { get; set; }
-        
-        public User UserId { get; set; }
+        public string   Name { get; set; }        
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
+
+        public User UserId { get; set; }
+        //lo pueden poner en Customer y se puede confundir con la clase si lo dejan customer
+        //saben que es una propiedad de relacion
+        public virtual ICollection<Customer> customer { get; set; }
 
     }
 }
--- ModernPOS.WebAPI/ModernPOS.WebAPI/Models/User.cs
+++ ModernPOS.WebAPI/ModernPOS.WebAPI/Models/User.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ModernPOS.WebAPI.Models
 {
-    class User
+    public class User
     {
         public int Id  { get; set; }
         public string Name { get; set; }
